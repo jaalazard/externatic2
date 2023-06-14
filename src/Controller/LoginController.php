64f279fdfe2aa5f3d9c8fffc59a04570daa433a1
jaalazard <ja.alazard@proton.me>
2023-06-14 15:22:14 +0200

    Gestion du logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,6 +25,6 @@ class LoginController extends AbstractController
     #[Route('/logout', name: 'app_logout')]
     public function logout(): never
     {
-        throw new \Exception('');
+        throw new Exception();
     }
 }
