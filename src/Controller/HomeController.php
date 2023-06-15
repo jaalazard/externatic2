8a703726389bc5fb47711c50aa03bf4f4e80e222
mathysfrc <mathysf41@gmail.com>
2023-06-15 16:56:31 +0200

    homepage company

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,19 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Consultant;
+use App\Repository\CompanyRepository;
 use App\Repository\ConsultantRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ConsultantRepository $consultantRepository): Response
+    public function index(ConsultantRepository $consultantRepository, CompanyRepository $companyRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'consultants' => $consultantRepository->findBy([], [
                 'firstname' => 'ASC',
             ]),
+            'companies' => $companyRepository->findBy([], [
+                'name' => 'ASC',
+            ])
         ]);
     }
+
+
 
     #[Route('/mentionslegales', name: 'mentionslegales')]
     public function mentionsLegales(): Response
