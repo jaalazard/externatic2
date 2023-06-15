96568118c15af9f297071b417558400af03ca7de
Aimery Pineau <aimery.pin@gmail.com>
2023-06-15 10:29:18 +0200

    page create job offer

---
+++ src/Form/JobOfferType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\JobOffer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class JobOfferType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('job', TextType::class, [
+                'label' => 'Métier',
+                'attr' => [
+                    'placeholder' => 'Sélectionner un métier pour cette offre',
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Ville',
+                ]
+            ])
+            ->add('contract', ChoiceType::class, [
+                'label' => 'Contrat',
+                'placeholder' => 'Sélectionner un contrat pour cette offre',
+
+
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'placeholder' => 'Décrivez cette offre',
+                    'multiple' => true,
+                    'expanded' => false, // If you want checkboxes instead of a select dropdown
+                ]
+            ])
+            ->add('createdAt', DateType::class, [
+                'label' => 'Date de la publication de cette offre',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => JobOffer::class,
+        ]);
+    }
+}
