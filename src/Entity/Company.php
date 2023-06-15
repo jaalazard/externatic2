17a5f85f6521f0db83a3b59ad94fe89d483923d1
mathysfrc <mathysf41@gmail.com>
2023-06-15 15:27:01 +0200

    edit crud company

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -4,8 +4,15 @@ namespace App\Entity;
 
 use App\Repository\CompanyRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Doctrine\DBAL\Types\Types;
+use Symfony\Component\Validator\Constraints as Assert;
+use DateTimeInterface;
+use DateTime;
 
 #[ORM\Entity(repositoryClass: CompanyRepository::class)]
+#[Vich\Uploadable]
 class Company
 {
     #[ORM\Id]
@@ -18,6 +25,20 @@ class Company
 
     #[ORM\Column(length: 255)]
     private ?string $link = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Assert\Length(max: 255)]
+    private ?string $poster = null;
+
+    #[Vich\UploadableField(mapping: 'company_file', fileNameProperty: 'poster')]
+    #[Assert\File(
+        maxSize: '1M',
+        mimeTypes: ['image/jpeg', 'image/png', 'image/webp'],
+    )]
+    private ?File $companyFile = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?DatetimeInterface $updatedAt = null;
 
     public function getId(): ?int
     {
@@ -44,6 +65,35 @@ class Company
     public function setLink(string $link): static
     {
         $this->link = $link;
+
+        return $this;
+    }
+
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(?string $poster): self
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+
+
+    public function getCompanyFile(): ?File
+    {
+        return $this->companyFile;
+    }
+
+    public function setCompanyFile(File $image = null): Company
+    {
+        $this->companyFile = $image;
+        if ($image) {
+            $this->updatedAt = new DateTime('now');
+        }
 
         return $this;
     }
