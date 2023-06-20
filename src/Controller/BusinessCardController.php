43305ac0051437d691f9998ed8cc9bb00f4cfada
mathysfrc <mathysf41@gmail.com>
2023-06-20 17:14:26 +0200

    modifs link

--- src/Controller/BusinessCardController.php
+++ src/Controller/BusinessCardController.php
@@ -7,10 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/business/card')]
+#[Route('/business/card', name: 'business-card')]
 class BusinessCardController extends AbstractController
 {
-    #[Route('/', name: 'app_business_card_index', methods: ['GET'])]
+    #[Route('/', name: '_index', methods: ['GET'])]
     public function index(BusinessRepository $businessRepository): Response
     {
         return $this->render('business-card/index.html.twig', [
