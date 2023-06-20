d433ffff1c9022534af9c76d2f6daa1788b44a5e
mathysfrc <mathysf41@gmail.com>
2023-06-20 12:09:08 +0200

    modifs required

--- src/Controller/BusinessCardController.php
+++ src/Controller/BusinessCardController.php
@@ -2,11 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\BusinessCard;
-use App\Form\BusinessCardType;
 use App\Repository\BusinessRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,66 +18,5 @@ class BusinessCardController extends AbstractController
                 'name' => 'ASC',
             ]),
         ]);
-    }
-
-    #[Route('/new', name: 'app_business_card_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, BusinessRepository $businessRepository): Response
-    {
-        $businessCard = new BusinessCard();
-        $form = $this->createForm(BusinessCardType::class, $businessCard);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $businessRepository->save($businessCard, true);
-
-            return $this->redirectToRoute('app_business_card_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('business_card/new.html.twig', [
-            'business_card' => $businessCard,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_business_card_show', methods: ['GET'])]
-    public function show(BusinessCard $businessCard): Response
-    {
-        return $this->render('business_card/show.html.twig', [
-            'business_card' => $businessCard,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_business_card_edit', methods: ['GET', 'POST'])]
-    public function edit(
-        Request $request,
-        BusinessCard $businessCard,
-        BusinessRepository $businessRepository
-    ): Response {
-        $form = $this->createForm(BusinessCardType::class, $businessCard);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $businessRepository->save($businessCard, true);
-
-            return $this->redirectToRoute('app_business_card_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('business_card/edit.html.twig', [
-            'business_card' => $businessCard,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_business_card_delete', methods: ['POST'])]
-    public function delete(
-        Request $request,
-        BusinessCard $businessCard,
-        BusinessRepository $businessRepository
-    ): Response {
-        if ($this->isCsrfTokenValid('delete' . $businessCard->getId(), $request->request->get('_token'))) {
-            $businessRepository->remove($businessCard, true);
-        }
-
-        return $this->redirectToRoute('app_business_card_index', [], Response::HTTP_SEE_OTHER);
     }
 }
