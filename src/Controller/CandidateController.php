496d340695f90d7796c1a9f9a109d41aa0fafcd5
jaalazard <ja.alazard@proton.me>
2023-07-02 17:21:59 +0200

    supprimer un skill as a candidate

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Candidate;
 use App\Entity\Formation;
+use App\Entity\Skill;
+use App\Entity\User;
 use App\Form\CandidateType;
 use App\Form\FormationType;
+use App\Repository\SkillRepository;
 use App\Repository\CandidateRepository;
 use App\Repository\FormationRepository;
 use Symfony\Component\HttpFoundation\Request;
@@ -33,6 +36,31 @@ class CandidateController extends AbstractController
         return $this->render('candidate/edit.html.twig', [
             'candidate' => $candidate, 'form' => $form,
         ]);
+    }
+
+    #[Route('/candidat/{id}/competence/supprimer', name: 'app_candidate_delete_skill', methods: ['POST'])]
+    public function deleteSkill(
+        Request $request,
+        Skill $skill,
+        SkillRepository $skillRepository
+    ): Response {
+        /** @var User */
+        $user = $this->getUser();
+        $candidate = $user->getCandidate();
+        if (
+            $this->isCsrfTokenValid(
+                'delete' . $skill->getId(),
+                $request->request->get('_token')
+            )
+        ) {
+            $skillRepository->remove($skill, true);
+        }
+
+        return $this->redirectToRoute(
+            'app_candidate_show',
+            ['candidate' => $candidate, 'id' => $candidate->getId()],
+            Response::HTTP_SEE_OTHER
+        );
     }
 
     #[Route('candidat/{id}', name: 'app_candidate_show', methods: ['GET', 'POST'])]
