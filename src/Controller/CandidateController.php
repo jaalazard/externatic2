3b521944e781ad7dab440e0d61fb04e51e14410a
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-07-03 16:57:47 +0200

    fixed tabs navbar

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -89,13 +89,10 @@ class CandidateController extends AbstractController
         );
     }
 
-    #[Route('candidat/{id}', name: 'app_candidate_show', methods: ['GET', 'POST'])]
-    public function show(Candidate $candidate): Response
+    #[Route('candidat', name: 'app_candidate_show', methods: ['GET', 'POST'])]
+    public function show(): Response
     {
-
-        return $this->render('candidate/show.html.twig', [
-            'candidate' => $candidate,
-        ]);
+        return $this->render('candidate/show.html.twig');
     }
 
     #[Route('candidat/{id}/formation', name: 'app_candidate_edit_formation')]
