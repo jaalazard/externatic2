fdc434d98a58f6fcdba4a119e00c9f3d23b91c1c
Aimery Pineau <aimery.pin@gmail.com>
2023-07-04 16:44:55 +0200

    affichage des cards offre d'emploi pour un cousultant

--- src/Controller/AdminConsultantController.php
+++ src/Controller/AdminConsultantController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\JobOffer;
 use App\Repository\JobOfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin/consultant', name: 'admin_consultant_')]
 class AdminConsultantController extends AbstractController
 {
-    #[Route('/admin/consultant', name: 'admin_consultant')]
+    #[Route('/', name: 'index')]
     public function index(JobOfferRepository $jobOfferRepository): Response
     {
         return $this->render('admin_consultant/index.html.twig', [
             'jobOffers' => $jobOfferRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(JobOffer $jobOffer, JobOfferRepository $jobOfferRepository): Response
+    {
+        return $this->render('admin_consultant/show.html.twig', [
+            'jobOffers' => $jobOfferRepository->findAll(),
+            'jobOffer' => $jobOffer,
         ]);
     }
 }
