b8e3de063ff9d372f14b522953181fe37e522d2e
mathysfrc <mathysf41@gmail.com>
2023-07-05 15:28:32 +0200

    favorite-page

---
+++ src/Controller/FavoriteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FavoriteController extends AbstractController
+{
+    #[Route('/favoris', name: 'app_favorite')]
+    public function index(): Response
+    {
+        return $this->render('favourite/index.html.twig', [
+            'controller_name' => 'FavouriteController',
+        ]);
+    }
+}
