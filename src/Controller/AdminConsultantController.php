66b8359ab5b5a1cd4f05d243d951fca82242d729
Aimery Pineau <aimery.pin@gmail.com>
2023-07-05 15:07:38 +0200

    index et show unifiés quelques modifs style

--- src/Controller/AdminConsultantController.php
+++ src/Controller/AdminConsultantController.php
@@ -11,20 +11,18 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/admin/consultant', name: 'admin_consultant_')]
 class AdminConsultantController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(JobOfferRepository $jobOfferRepository): Response
+    #[Route('/{id?null}', name: 'index', methods: ['GET'])]
+    public function index(string $id, JobOfferRepository $jobOfferRepository): Response
     {
+        if (!empty($id)) {
+            $selectedJobOffer = $jobOfferRepository->findOneBy(['id' => $id]);
+        } else {
+            $selectedJobOffer = null;
+        }
         return $this->render('admin_consultant/index.html.twig', [
             'jobOffers' => $jobOfferRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/{id}', name: 'show', methods: ['GET'])]
-    public function show(JobOffer $jobOffer, JobOfferRepository $jobOfferRepository): Response
-    {
-        return $this->render('admin_consultant/show.html.twig', [
-            'jobOffers' => $jobOfferRepository->findAll(),
-            'jobOffer' => $jobOffer,
+            'selectedJobOffer' => $selectedJobOffer,
+            'id' => $id,
         ]);
     }
 }
