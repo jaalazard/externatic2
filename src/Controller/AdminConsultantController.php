1ca97a33fa8f3c9e4a7530c7d61962e8c714635e
Aimery Pineau <aimery.pin@gmail.com>
2023-07-06 10:00:34 +0200

    dscscs

--- src/Controller/AdminConsultantController.php
+++ src/Controller/AdminConsultantController.php
@@ -11,18 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/admin/consultant', name: 'admin_consultant_')]
 class AdminConsultantController extends AbstractController
 {
-    #[Route('/{id?null}', name: 'index', methods: ['GET'])]
-    public function index(string $id, JobOfferRepository $jobOfferRepository): Response
+    #[Route('/{id}', name: 'index', methods: ['GET'])]
+    public function index(?JobOffer $jobOffer = null, JobOfferRepository $jobOfferRepository): Response
     {
-        if (!empty($id)) {
-            $selectedJobOffer = $jobOfferRepository->findOneBy(['id' => $id]);
-        } else {
-            $selectedJobOffer = null;
-        }
         return $this->render('admin_consultant/index.html.twig', [
             'jobOffers' => $jobOfferRepository->findAll(),
-            'selectedJobOffer' => $selectedJobOffer,
-            'id' => $id,
+            'jobOffer' => $jobOffer,
         ]);
     }
 }
