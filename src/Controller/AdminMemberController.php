3fd4d632453736664297d5aa4e8c0e36e70e1bb5
jaalazard <ja.alazard@proton.me>
2023-07-10 14:13:32 +0200

    en tant qu'admin je veux une page listant mes users

---
+++ src/Controller/AdminMemberController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/membres', name: 'admin_members_')]
+class AdminMemberController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('admin_membres/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+}
