e52d54628b2e0cb9f43ee033305b43e78e3dbc87
Aimery Pineau <aimery.pin@gmail.com>
2023-07-10 14:12:17 +0200

    filtrage des offres d'emploi search

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,6 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
