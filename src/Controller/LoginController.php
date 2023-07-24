dd54ab331c83fa31367ae0d9e971758662ef8a3c
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-07-24 15:51:58 +0200

    Add Flash Connexion Admin/candidate

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use Exception;
+use App\Entity\Candidate;
+use App\Form\CandidateType;
+use App\Repository\CandidateRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
